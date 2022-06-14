b074696ea93c23cb94cd2254bf5c3475878ec19d
Demian Danhof <demiandanhof@gmail.com>
2022-06-14 16:23:32 +0200

    Styling voor de vraag pagina is af. In index.css staat nu de styling voor de knoppen

--- src/components/layout/Questions/QuestionsList.js
+++ src/components/layout/Questions/QuestionsList.js
@@ -3,16 +3,32 @@ import { useDispatch, useSelector } from "react-redux";
 
 import { getAllQuestions } from "../../../actions/QuestionActions";
 
+import "./Question.css";
+
 export const QuestionsList = () => {
   const allQuestions = useSelector((state) => state.questions.allQuestions);
   const dispatch = useDispatch();
   return (
-    <ul>
-      <button onClick={() => dispatch(getAllQuestions())}> GetAll </button>
+    <section className="questionWrapper">
+      <section className="imgWrapperQuestion">
+        <img src="/img/question-mark.png" className="questionImg"/>
+      </section>
+      <section className="questionWrapperGrid">
+        <section className="sectionLeftQuestion">
+            {/* <button onClick={() => dispatch(getAllQuestions())}> GetAll </button> */}
+            <h1 className="orange-text text-align-center">Vraag</h1>
 
-      {allQuestions.map((question) => (
-        <li key={question.id}> {question.question} </li>
-      ))}
-    </ul>
+            {allQuestions.map((question) => (
+              <p className="text-align-center" key={question.id}> {question.question}</p>
+            ))}
+        </section>
+        <section className="sectionRightQuestion">
+          <button className="answer-button">Antwoord 1</button>
+          <button className="answer-button">Antwoord 2</button>
+          <button className="answer-button">Antwoord 3</button>
+          <button className="answer-button">Antwoord 4</button>
+        </section>
+      </section>
+    </section>
   );
 };
