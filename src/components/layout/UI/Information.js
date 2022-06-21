a7b56b99c02e5fd3b71bb091ffa3dab9a69312b4
Demian Danhof <demiandanhof@gmail.com>
2022-06-21 14:30:53 +0200

    Intro tekstje geschreven

--- src/components/layout/UI/Information.js
+++ src/components/layout/UI/Information.js
@@ -11,15 +11,16 @@ export const Information = () => {
     <section className="informationWrapper">
       <img src="/img/info.png" className="questionImg" alt="Information"></img>
       <section className="sectionInformationText">
-        <h1 className="orange-text text text-align-center">Titel</h1>
+        <h1 className="orange-text text text-align-center">Welkom!</h1>
         <p className="text-align-center">
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-          aliquip ex ea commodo consequat. Duis aute irure dolor in
-          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-          culpa qui officia deserunt mollit anim id est laborum.
+          Leuk dat je de PostNL Equal Flow loopbaan elearning gaat volgen! 
+          Deze elearning heeft als doel je wat bij te leren over een 'Equal flow' op de exportbaan in het distributiecentrum.
+          De elearning bestaat uit een aantal vragen omtrent het behouden van een 'Equal flow' op de exportbaan. Hoe verdeel je je personeel het beste? Wat te doen als je personeel te kort komt?
+          Na deze elearning zal je een beter beeld hebben over deze 'Equal flow'.
+          Na het beantwoorden van een vraag, wordt het juiste antwoord groen en kun je onder de vraag zien waarom dat het juiste antwoord is.
+          Bij een goed antwoord krijg je punten. Je totaal score zie je linksboven. Daarnaast kun je altijd je progressie zien in de balk onderaan.
+          Je kan tussentijds stoppen met de elearning, je progressie wordt automatisch opgeslagen.
+          Veel plezier en succes tijdens deze elearning!
         </p>
       </section>
       <button
